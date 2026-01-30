264209c367fbf6a72c6ad18a25751f14fbbeab81
jenish-vaghani-teqnodux <jenish.vaghani@teqnodux.com>
2026-01-30 17:09:42 +0530

    @vercel/node delete

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,11 @@
 import express from 'express'
-import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 const app = express()
+
 app.use(express.json())
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK' })
+    res.json({status: 'OK'})
 })
 
-// 🔥 VERY IMPORTANT PART
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  app(req, res)
-}
+export default app
