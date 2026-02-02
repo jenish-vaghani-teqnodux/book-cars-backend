0d5612eb82eadee70848fac9019dd1e85be830eb
jenish-vaghani-teqnodux <jenish.vaghani@teqnodux.com>
2026-02-02 11:37:04 +0530

    change import

--- api/index.ts
+++ api/index.ts
@@ -2,15 +2,13 @@ let cached: any
 
 export default async function handler(req: any, res: any) {
   if (!cached) {
-    await import('tsconfig-paths/register')
     // @ts-ignore
     cached = import('../dist/src/app.js')
   }
 
   const mod: any = await cached
-  if (mod.initPromise) 
-    {
-      await mod.initPromise
-    }
-      return mod.default(req, res)
+  if (mod.initPromise) {
+    await mod.initPromise
+  }
+  return mod.default(req, res)
 }
