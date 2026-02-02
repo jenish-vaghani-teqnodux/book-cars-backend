8f2ca9a62612d928429b417bc57311958071103d
jenish-vaghani-teqnodux <jenish.vaghani@teqnodux.com>
2026-02-02 11:18:28 +0530

    new changes

--- api/index.ts
+++ api/index.ts
@@ -2,14 +2,15 @@ let cached: any
 
 export default async function handler(req: any, res: any) {
   if (!cached) {
-    // dist build pachhi j exist thase; build time ma TS ne ignore karavvu
+    await import('tsconfig-paths/register')
     // @ts-ignore
     cached = import('../dist/src/app.js')
   }
 
   const mod: any = await cached
-  if (mod.initPromise) {
-    await mod.initPromise
-  }
-    return mod.default(req, res)
+  if (mod.initPromise) 
+    {
+      await mod.initPromise
+    }
+      return mod.default(req, res)
 }
