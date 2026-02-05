e5b8c3454cd72499786746c9c7646a2013b62fe9
jenish-vaghani-teqnodux <jenish.vaghani@teqnodux.com>
2026-02-05 12:35:20 +0530

    secure true

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -161,7 +161,7 @@ export const AUTH_COOKIE_DOMAIN = __env__('BC_AUTH_COOKIE_DOMAIN', false, 'local
  *
  * @type {CookieOptions}
  */
-export const COOKIE_OPTIONS: CookieOptions = { httpOnly: true, secure: HTTPS, signed: true, sameSite: 'none', path: '/' }
+export const COOKIE_OPTIONS: CookieOptions = { httpOnly: true, secure: true, signed: true, sameSite: 'none', path: '/' }
 
 /**
  * frontend authentication cookie name.
